b9fc4c225750d83a16eb1815d807e883403874a3
orjandesmet <orjan.de.smet@gmail.com>
2019-02-13 19:39:28 +0100

    Exercise 3: Solution

--- src/car-assembly-line.ts
+++ src/car-assembly-line.ts
@@ -1,5 +1,5 @@
 import { timer } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Car, CarColor } from './model/car';
 
 export class CarAssemblyLine {
@@ -10,12 +10,10 @@ export class CarAssemblyLine {
         console.log('CarAssemblyLine', `START_CREATING_CAR`, color);
         return timer(this.TIME_TO_CREATE_LINE)
             .pipe(
-                tap(() => { // EX03: Change this into a map operator and return the built car
-                    Car.build({ color });
-                }),
-                tap(timerCount => { // EX03: This tap operator will have the car as value instead of the timerCount
-                    const chassisNumber = `${timerCount}`;
-                    console.log('CarAssemblyLine', 'FINISHED_CREATING_CAR', color, chassisNumber);
+                map(() => Car.build({ color })),
+                tap(car => {
+                    const chassisNumber = car.chassisNumber;
+                    console.log('CarAssemblyLine', 'FINISHED_CREATING_CAR', car.color, chassisNumber);
                 }));
     }
 }
