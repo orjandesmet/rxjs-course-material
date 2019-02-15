a6c399adb9144b020b8b55abc26c75cd4ac48a7d
orjandesmet <orjan.de.smet@gmail.com>
2019-02-15 16:05:47 +0100

    Exercise 5: Solution

--- src/car-assembly-line.ts
+++ src/car-assembly-line.ts
@@ -1,5 +1,5 @@
-import { Observable, timer } from 'rxjs';
-import { concatMap, delay, map, scan, take, tap } from 'rxjs/operators';
+import { Observable, timer, zip } from 'rxjs';
+import { concatMap, delay, map, scan, tap } from 'rxjs/operators';
 import { Car, CarColor } from './model/car';
 import { Seat } from './model/seat';
 import { SteeringWheel } from './model/steering-wheel';
@@ -11,18 +11,10 @@ export class CarAssemblyLine {
     constructor(private paintShop: PaintShop) { }
 
     createCarOnLine(color: CarColor) {
-        return timer(0, 7000)
+        console.log('CarAssemblyLine', `START_CREATING_CAR`, color);
+        return zip(this.createChassis(), this.createWheels(), this.createSteeringWheel(), this.createSeats())
             .pipe(
-                tap(() => console.log('CarAssemblyLine', `START_CREATING_CAR`, color)),
-                concatMap(() => this.createChassis().pipe(
-                    concatMap(chassisNr => this.createWheels()
-                        .pipe(concatMap(wheels => this.createSteeringWheel()
-                            .pipe(concatMap(steeringWheel => this.createSeats()
-                                .pipe(concatMap(seats => this.assembleCar(chassisNr, wheels, steeringWheel, seats)))
-                            ))
-                        ))
-                    )),
-                ),
+                concatMap(([chassisNr, wheels, steeringWheel, seats]) => this.assembleCar(chassisNr, wheels, steeringWheel, seats)),
                 concatMap(car => this.paintShop.paintCar(car, color)),
                 map(car => ({ car, counter: 1 })),
                 scan((acc, next) => ({ car: next.car, counter: acc.counter + 1 }), { car: null, counter: 0 }),
@@ -34,7 +26,6 @@ export class CarAssemblyLine {
 
     private createChassis(): Observable<string> {
         return timer(0, 1000).pipe(
-            take(1),
             delay(300),
             tap(() => console.log('Chassis', 'STARTED')),
             delay(700),
@@ -45,7 +36,6 @@ export class CarAssemblyLine {
 
     private createWheels(): Observable<Wheel[]> {
         return timer(0, 1000).pipe(
-            take(1),
             delay(300),
             tap(() => console.log('Wheels', 'STARTED')),
             delay(700),
@@ -56,7 +46,6 @@ export class CarAssemblyLine {
 
     private createSteeringWheel(): Observable<SteeringWheel> {
         return timer(0, 1000).pipe(
-            take(1),
             delay(300),
             tap(() => console.log('SteeringWheel', 'STARTED')),
             delay(700),
@@ -67,7 +56,6 @@ export class CarAssemblyLine {
 
     private createSeats(): Observable<Seat[]> {
         return timer(0, 1000).pipe(
-            take(1),
             delay(300),
             tap(() => console.log('Seats', 'STARTED')),
             delay(700),
