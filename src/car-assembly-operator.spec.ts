b729752f1e072705fafa78a884db265f9a86bd76
orjandesmet <orjan.de.smet@gmail.com>
2019-03-02 09:34:17 +0100

    Exercise 8: Solution Bonus

--- src/car-assembly-operator.spec.ts
+++ src/car-assembly-operator.spec.ts
@@ -1,11 +1,25 @@
+import { Subject } from 'rxjs';
 import { marbles } from 'rxjs-marbles';
 import { carAssemblyOperator, multiCarAssemblyOperator } from './car-assembly-operator';
 
 describe('Car Assembly Operators', () => {
     let consoleLogSpy: jest.SpyInstance;
+    let consoleLogSubject$: Subject<any>;
+    const consoleLogValues = {
+        s: {message: 'Something', 0: 1, 1: 'STARTED' },
+        f: {message: 'Something', 0: 1, 1: 'FINISHED', 2: 'SOMETHING'},
+        e: {message: 'Something', 0: 1, 1: 'RESOLVED_ERROR', 2: 'error'}
+    };
+    const multiConsoleLogValues = {
+        s: {message: 'Something', 0: 1, 1: 'STARTED' },
+        f: {message: 'Something', 0: 1, 1: 'FINISHED', 2: {count: 1, text: 'SOMETHING' }},
+        e: {message: 'Something', 0: 1, 1: 'RESOLVED_ERROR', 2: 'error'}
+    };
 
     beforeEach(() => {
-        consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+        consoleLogSubject$ = new Subject();
+        consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(() => {})
+            .mockImplementation((message: string, ...args: string[]) => consoleLogSubject$.next({message, ...args}));
         consoleLogSpy.mock.calls = [];
     });
 
@@ -17,11 +31,7 @@ describe('Car Assembly Operators', () => {
 
             const result$ = carAssemblyOperator(1)(name, time, creator)(m.cold('x|', {x: undefined}));
             m.expect(result$).toBeObservable('300ms ---(s|)', {s: 'SOMETHING'});
-            m.flush();
-            expect(consoleLogSpy).toHaveBeenCalledTimes(2);
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'STARTED');
-            expect(consoleLogSpy).not.toHaveBeenCalledWith('Something', 1, 'RESOLVED_ERROR', 'error');
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'FINISHED', 'SOMETHING');
+            m.expect(consoleLogSubject$).toBeObservable('100ms s 200ms --f', consoleLogValues);
         }));
 
         it('should return EMPTY on error', marbles(m => {
@@ -31,11 +41,7 @@ describe('Car Assembly Operators', () => {
 
             const result$ = carAssemblyOperator(1)(name, time, creator)(m.cold('x|', {x: undefined}));
             m.expect(result$).toBeObservable('300ms ---|', {s: 'SOMETHING'});
-            m.flush();
-            expect(consoleLogSpy).toHaveBeenCalledTimes(2);
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'STARTED');
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'RESOLVED_ERROR', 'error');
-            expect(consoleLogSpy).not.toHaveBeenCalledWith('Something', 1, 'FINISHED', 'SOMETHING');
+            m.expect(consoleLogSubject$).toBeObservable('100ms s 200ms --e', consoleLogValues);
         }));
     });
 
@@ -48,11 +54,7 @@ describe('Car Assembly Operators', () => {
 
             const result$ = multiCarAssemblyOperator<T>(1)<T>(name, time, creator, 3)(m.cold('x 300ms x 300ms x|', {x: undefined}));
             m.expect(result$).toBeObservable('900ms -----(s|)', {s: [{count: 1, text: 'SOMETHING' }, {count: 2, text: 'SOMETHING' }, {count: 3, text: 'SOMETHING' }]});
-            m.flush();
-            expect(consoleLogSpy).toHaveBeenCalledTimes(6);
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'STARTED');
-            expect(consoleLogSpy).not.toHaveBeenCalledWith('Something', 1, 'RESOLVED_ERROR', 'error');
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'FINISHED', {count: 1, text: 'SOMETHING' });
+            m.expect(consoleLogSubject$).toBeObservable('100ms s 200ms --f 97ms s 200ms --f 97ms s 200ms --f', multiConsoleLogValues);
         }));
 
         it('should return EMPTY on error', marbles(m => {
@@ -63,11 +65,7 @@ describe('Car Assembly Operators', () => {
 
             const result$ = multiCarAssemblyOperator<T>(1)<T>(name, time, creator, 3)(m.cold('x 300ms x 300ms x|', {x: undefined}));
             m.expect(result$).toBeObservable('900ms -----|', {s: [{count: 1, text: 'SOMETHING' }, {count: 2, text: 'SOMETHING' }, {count: 3, text: 'SOMETHING' }]});
-            m.flush();
-            expect(consoleLogSpy).toHaveBeenCalledTimes(6);
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'STARTED');
-            expect(consoleLogSpy).toHaveBeenCalledWith('Something', 1, 'RESOLVED_ERROR', 'error');
-            expect(consoleLogSpy).not.toHaveBeenCalledWith('Something', 1, 'FINISHED', 'SOMETHING');
+            m.expect(consoleLogSubject$).toBeObservable('100ms s 200ms --e 97ms s 200ms --e 97ms s 200ms --e', multiConsoleLogValues);
         }));
     });
 });
